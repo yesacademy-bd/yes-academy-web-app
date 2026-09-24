08cda3231f6244fa88b386982cf99d8ba5134f74
yesacademy-bd <soumik.yesacademy@gmail.com>
2026-09-24 17:56:41 +0600

    chore: remove test file

--- test.ts
+++
@@ -1,8 +0,0 @@
-import { createAdminClient } from '@/utils/supabase/admin'
-
-async function check() {
-  const adminClient = createAdminClient()
-  const { data, error } = await adminClient.from('mock_services').select('service_type, mock_type, test_type').limit(10)
-  console.log(data)
-}
-check()
